e0afebce705925802d8a74dd2645906734688476
Hushuo <s1082821@gm.pu.edu.tw>
2021-05-28 14:54:52 +0800

    2021上機考v2_胡琪碩

--- app/src/main/java/tw/edu/pu/s1082821/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1082821/crazyshape/MainActivity.kt
@@ -6,6 +6,11 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +18,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Toast.makeText(baseContext, "作者：胡琪碩", Toast.LENGTH_LONG).show()
+
+        GlideApp.with(this)
+            .load(R.drawable.cover)
+            .override(800, 600)
+            .into(imgView)
 
         imgNext.setOnLongClickListener(object : View.OnLongClickListener {
             override fun onLongClick(p0: View?): Boolean {
@@ -23,3 +33,4 @@ class MainActivity : AppCompatActivity() {
         })
     }
 }
+
