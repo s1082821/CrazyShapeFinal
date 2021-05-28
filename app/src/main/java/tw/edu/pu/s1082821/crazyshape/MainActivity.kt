03f4d8246889b30bf4df4a6c42e02c24c255b3a2
Hushuo <s1082821@gm.pu.edu.tw>
2021-05-28 14:59:37 +0800

    2021上機考v3_胡琪碩

--- app/src/main/java/tw/edu/pu/s1082821/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1082821/crazyshape/MainActivity.kt
@@ -13,16 +13,36 @@ public final class MyAppGlideModule : AppGlideModule()
 
 
 class MainActivity : AppCompatActivity() {
+
+    var Flag:Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Toast.makeText(baseContext, "作者：胡琪碩", Toast.LENGTH_LONG).show()
+        rndShape()
 
         GlideApp.with(this)
             .load(R.drawable.cover)
             .override(800, 600)
             .into(imgView)
+
+        imgNext.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(p0: View?) {
+                rndShape()
+            }
+        })
+    }
+
+    fun rndShape(){
+        Flag = (1..4).random()
+        when (Flag){
+            1->imgNext.setImageResource(R.drawable.circle)
+            2->imgNext.setImageResource(R.drawable.square)
+            3->imgNext.setImageResource(R.drawable.star)
+            4->imgNext.setImageResource(R.drawable.triangle)
+        }
 
         imgNext.setOnLongClickListener(object : View.OnLongClickListener {
             override fun onLongClick(p0: View?): Boolean {
